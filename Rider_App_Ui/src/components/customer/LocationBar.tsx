e015727b6b84780931b15ebfdd3c6f5c361bdf0f
Sanjay Kumar <sanjubabu9419@gmail.com>
2026-04-08 18:09:54 +0530

    Update LocationBar.tsx

--- Rider_App_Ui/src/components/customer/LocationBar.tsx
+++ Rider_App_Ui/src/components/customer/LocationBar.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { useWS } from "@/service/WSProvider";
 import { useUserStore } from "@/store/userStore";
 import { uiStyles } from "@/styles/uiStyles";
+import { logout } from "@/service/authService";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Colors } from "@/utils/Constants";
 
 const LocationBar = () => {
   const { location } = useUserStore();
   const { disconnect } = useWS();
   return (
     <View style={uiStyles.absoluteTop}>
+      <View style={uiStyles.container}>
+        <TouchableOpacity
+          style={uiStyles.btn}
+          onPress={() => logout(disconnect)}
+        >
+          <Ionicons name="menu" size={RFValue(18)} color={Colors.text} />
+        </TouchableOpacity>
+      </View>
       <Text>LocationBar</Text>
     </View>
   );
